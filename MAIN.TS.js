4e0bb54abaa1425eeee81ccba2d04d09b43bb6be
Shazia91 <164497525+Shazia91@users.noreply.github.com>
2024-03-22 17:42:01 +0500

    Add files via upload

---
+++ MAIN.TS.js
@@ -0,0 +1,20 @@
+//CLASS # 1
+//DATA TYPES PRIMITIVES
+//NUMBER
+var A = 123;
+console.log(A);
+//STRING
+var message = "hello world!";
+console.log(message);
+// BOOLEAN
+var C = true;
+console.log(C);
+//NULL
+var D = null;
+console.log(D);
+// UNDEFINED
+var E;
+console.log(E);
+//symbol
+var F;
+console.log(F);
